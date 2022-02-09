8e98101b2ce69fb03c5d319d2cd9ecb43472fe7d
mohaimenurrahman <mohaimenur888@gmail.com>
2022-02-09 16:32:52 +0600

    Task

---
+++ Task/News/News/Controllers/NewsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using News.Models.Database;
+
+namespace News.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            DBEntities db = new DBEntities();
+            var data = db.News;
+            return View(data.ToList());
+        }
+
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(News.Models.Database.News n)
+        {
+            if(ModelState.IsValid)
+            {
+                DBEntities db = new DBEntities();
+                db.News.Add(n);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(n);
+        }
+
+        public ActionResult Edit()
+        {
+
+            return View();
+        }
+    }
+}
